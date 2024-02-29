9ed4c1a8a94d57ca00e4016a17da2baa048f7161
Carlos Zambrana <czambrana@mnahealthcare.com>
2024-02-28 23:58:41 -0600

    Add admin customization for Blog model and create migration for author field

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-
+from django.utils.html import format_html
 from .models import Blog
 
-admin.site.register(Blog)
+#show title, date, author, excerpt, and image in the admin panel
+class BlogAdmin(admin.ModelAdmin):
+    list_display = ('title', 'pub_date_pretty', 'author', 'summary', 'image_tag')
+    
+    def image_tag(self, obj):
+        return format_html('<img src="{}" width="96" />'.format(obj.image.url))
+
+    image_tag.short_description = 'Image'
+
+
+admin.site.register(Blog, BlogAdmin)
+
--- blog/models.py
+++ blog/models.py
@@ -1,11 +1,15 @@
 from django.db import models
 
+
+
 # create blog models here
 class Blog(models.Model):
     title = models.CharField(max_length=200)
     pub_date = models.DateTimeField()
     body = models.TextField()
     image = models.ImageField(upload_to='images/')
+    # add author field to the blog model related to the user model
+    author = models.ForeignKey('auth.User', on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
@@ -15,3 +19,4 @@ class Blog(models.Model):
 
     def pub_date_pretty(self):
         return self.pub_date.strftime('%b %e %Y')
+    
